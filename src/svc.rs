19b7219b5ede8dd11093b9121d0cee5ccb5411a4
Kevin Parsons <kevpar@microsoft.com>
2022-06-16 00:15:56 -0700

    Pull out windows_service stuff in svc.rs

--- src/svc.rs
+++ src/svc.rs
@@ -8,10 +8,20 @@ use std::{
     sync::Arc,
     time::Duration,
 };
-use windows_service::{
-    service::{ServiceControl, ServiceControlAccept, ServiceState},
-    service_control_handler::{self, ServiceControlHandlerResult, ServiceStatusHandle},
-};
+
+// windows_service type aliases.
+// Maybe replace these with our own abstraction in the future.
+type Result<T> = windows_service::Result<T>;
+type Error = windows_service::Error;
+type ServiceStatusHandle = windows_service::service_control_handler::ServiceStatusHandle;
+type ServiceControlHandlerResult =
+    windows_service::service_control_handler::ServiceControlHandlerResult;
+type ServiceControl = windows_service::service::ServiceControl;
+type ServiceControlAccept = windows_service::service::ServiceControlAccept;
+type ServiceState = windows_service::service::ServiceState;
+type ServiceStatus = windows_service::service::ServiceStatus;
+type ServiceType = windows_service::service::ServiceType;
+type ServiceExitCode = windows_service::service::ServiceExitCode;
 
 pub struct Service {
     config: config::Config,
@@ -26,12 +36,12 @@ impl Service {
         status_handle: ServiceStatusHandle,
         status: ServiceState,
         controls_accepted: ServiceControlAccept,
-    ) -> windows_service::Result<()> {
-        status_handle.set_service_status(windows_service::service::ServiceStatus {
-            service_type: windows_service::service::ServiceType::OWN_PROCESS,
+    ) -> Result<()> {
+        status_handle.set_service_status(ServiceStatus {
+            service_type: ServiceType::OWN_PROCESS,
             current_state: status,
             controls_accepted: controls_accepted,
-            exit_code: windows_service::service::ServiceExitCode::Win32(0),
+            exit_code: ServiceExitCode::Win32(0),
             checkpoint: 0,
             wait_hint: Duration::default(),
             process_id: None,
@@ -42,9 +52,7 @@ impl Service {
         self.run_inner().unwrap();
     }
 
-    fn output_stream(
-        config: &config::OutputStream,
-    ) -> windows_service::Result<std::process::Stdio> {
+    fn output_stream(config: &config::OutputStream) -> Result<std::process::Stdio> {
         match config {
             config::OutputStream::Null => Ok(std::process::Stdio::null()),
             config::OutputStream::File {
@@ -62,9 +70,7 @@ impl Service {
                         oo.truncate(true);
                     }
                 }
-                let f = oo
-                    .open(path)
-                    .map_err(|err| windows_service::Error::Winapi(err))?;
+                let f = oo.open(path).map_err(|err| Error::Winapi(err))?;
                 Ok(Into::into(f))
             }
         }
@@ -74,17 +80,19 @@ impl Service {
     // service_control (winsvc?) provides all special bindings to tie to SCM
     // svc provides core concept of a runnable service, receives some abstraction over control handling and setting service status
     //   should be something we can pull out and test with a mock SCM
+    //
+    // outer wrapper talks SCM concepts to SCM
+    // inner wrapper
 
-    fn run_inner(&self) -> windows_service::Result<()> {
+    fn run_inner(&self) -> Result<()> {
         let (tx, rx) = crossbeam_channel::bounded(0);
         let handler = ServiceControlHandler::new(tx.clone());
-        let status_handle =
-            service_control_handler::register(&self.config.registration.name, move |sc| {
-                handler.handle(sc)
-            })?;
+        let status_handle = windows_service::service_control_handler::register(
+            &self.config.registration.name,
+            move |sc| handler.handle(sc),
+        )?;
 
-        let job =
-            jobobjects::JobObject::new().map_err(|err| windows_service::Error::Winapi(err))?;
+        let job = jobobjects::JobObject::new().map_err(|err| Error::Winapi(err))?;
         let mut limits = jobobjects::ExtendedLimitInformation::new();
         limits.set_kill_on_close();
         if let Some(job_object_config) = &self.config.job_object {
@@ -93,22 +101,20 @@ impl Service {
             }
         }
         job.set_extended_limits(limits)
-            .map_err(|err| windows_service::Error::Winapi(err))?;
-        job.add_self()
-            .map_err(|err| windows_service::Error::Winapi(err))?;
+            .map_err(|err| Error::Winapi(err))?;
+        job.add_self().map_err(|err| Error::Winapi(err))?;
 
         let mut c = Command::new(&self.config.process.binary);
         c.args(&self.config.process.args);
         c.envs(&self.config.process.environment);
         if let Some(wd) = &self.config.process.working_directory {
-            fs::create_dir_all(wd).map_err(|err| windows_service::Error::Winapi(err))?;
+            fs::create_dir_all(wd).map_err(|err| Error::Winapi(err))?;
             c.current_dir(wd);
         }
         c.stdout(Service::output_stream(&self.config.process.stdout)?);
         c.stderr(Service::output_stream(&self.config.process.stderr)?);
 
-        let child =
-            SharedChild::spawn(&mut c).map_err(|err| windows_service::Error::Winapi(err))?;
+        let child = SharedChild::spawn(&mut c).map_err(|err| Error::Winapi(err))?;
         let child = Arc::new(child);
         let waiter_child = child.clone();
         println!("child started with pid {}", child.id());
