0614d819f6cce389ff9760eae613874794675015
Kevin Parsons <kevpar@microsoft.com>
2022-06-16 22:59:32 -0700

    service_control: ServiceEntry FnMut -> Fn

--- src/service_control.rs
+++ src/service_control.rs
@@ -15,7 +15,7 @@ define_windows_service!(ffi_service_main, service_main);
 
 struct ServiceEntry {
     name: String,
-    runner: Box<dyn FnMut(ServiceControlHandler) + Send>,
+    runner: Box<dyn Fn(ServiceControlHandler) + Send>,
 }
 
 /// Stores the global information needed to run the service.
@@ -23,14 +23,14 @@ struct ServiceEntry {
 static SERVICE_TABLE: OnceCell<Mutex<ServiceEntry>> = OnceCell::new();
 
 fn service_main(_args: Vec<OsString>) {
-    let mut s = SERVICE_TABLE.get().unwrap().lock().unwrap();
+    let s = SERVICE_TABLE.get().unwrap().lock().unwrap();
     let handler = ServiceControlHandler::new(&s.name).unwrap();
     (s.runner)(handler);
 }
 
 pub fn register_service(
     name: String,
-    runner: Box<dyn FnMut(ServiceControlHandler) + Send>,
+    runner: Box<dyn Fn(ServiceControlHandler) + Send>,
 ) -> std::result::Result<(), String> {
     SERVICE_TABLE
         .set(Mutex::new(ServiceEntry { name, runner }))
